6f752ba978c1557e48cafaafa4d5fc88f7a3dcfa
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-15 22:30:27 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -9,30 +9,31 @@ BOT_TOKEN = '5619054777:AAF_XsEHxhJ7aXRRKowWzCR6R2u3vC1Hsi8'
 # Create the Pyrogram client
 app = Client('my_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-# Define the ban_all command handler
-@app.on_message(filters.command('banall'))
+# Define the /banall command handler
+@app.on_message(filters.command("banall") & filters.group)
 def ban_all_command_handler(client, message):
-    # Get the chat ID and the user ID of the person who triggered the command
     chat_id = message.chat.id
     user_id = message.from_user.id
-    
-    # Check if the user is an administrator in the chat
     chat_member = client.get_chat_member(chat_id, user_id)
-    if chat_member.status not in ['creator', 'administrator']:
-        message.reply('You must be an admin to use this command.')
-        return
     
-    # Get the list of group members
-    members = client.get_chat_members(chat_id)
-    
-    # Ban each group member
-    for member in members:
-        user_id = member.user.id
-        if user_id != message.from_user.id:
-            client.restrict_chat_member(chat_id, user_id, ChatPermissions())
-    
-    # Send a confirmation message
-    message.reply('All group members have been banned.')
+    if chat_member.status in ["administrator", "creator"]:
+        # Get all members in the chat
+        members = client.get_chat_members(chat_id)
+        
+        # Ban all members except for the bot and admins
+        for member in members:
+            member_id = member.user.id
+            member_chat_member = client.get_chat_member(chat_id, member_id)
+            if not member_bot and member_chat_member.status not in ["administrator", "creator"]:
+                client.kick_chat_member(chat_id, member_id)
+        
+        # Send a message to confirm the ban
+        message.reply_text("All members except for admins and the bot have been banned.")
+        
+    else:
+        # If the user is not an admin, send an error message
+        message.reply_text("You must be an admin to use this command.")
+
 
 # Start the bot
 app.run()
