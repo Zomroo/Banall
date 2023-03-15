2032ff78392ecd3c3167dbf37656d84af91f16ce
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-15 22:16:53 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -1,33 +1,37 @@
-import telegram
-from telegram.ext import CommandHandler, Updater
+from pyrogram import Client, filters
+from pyrogram.types import ChatPermissions
 
 # Define your bot token here
-TOKEN = '5619054777:AAF_XsEHxhJ7aXRRKowWzCR6R2u3vC1Hsi8'
+API_ID = 15849735
+API_HASH = 'b8105dc4c17419dfd4165ecf1d0bc100'
+BOT_TOKEN = '5619054777:AAF_XsEHxhJ7aXRRKowWzCR6R2u3vC1Hsi8'
 
-def ban_all(update, context):
-    # Get the chat ID and bot object
-    chat_id = update.message.chat_id
-    bot = context.bot
+# Create the Pyrogram client
+app = Client('my_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# Define the ban_all command handler
+@app.on_message(filters.command('banall'))
+def ban_all_command_handler(client, message):
+    # Get the chat ID and the user ID of the person who triggered the command
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    
+    # Check if the user is an administrator in the chat
+    chat_member = client.get_chat_member(chat_id, user_id)
+    if chat_member.status not in ['creator', 'administrator']:
+        message.reply('You must be an admin to use this command.')
+        return
     
     # Get the list of group members
-    members = bot.get_chat_members(chat_id)
+    members = client.get_chat_members(chat_id)
     
     # Ban each group member
     for member in members:
         user_id = member.user.id
-        bot.kick_chat_member(chat_id, user_id)
+        client.restrict_chat_member(chat_id, user_id, ChatPermissions())
     
     # Send a confirmation message
-    bot.send_message(chat_id=chat_id, text='All group members have been banned.')
-
-# Create the updater and dispatcher
-updater = Updater(TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-# Register the ban_all command handler
-ban_all_handler = CommandHandler('banall', ban_all)
-dispatcher.add_handler(ban_all_handler)
+    message.reply('All group members have been banned.')
 
 # Start the bot
-updater.start_polling()
-updater.idle()
+app.run()
