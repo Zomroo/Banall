d574957891b9f1963eac28d136b756939dd8eb42
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 00:52:58 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -4,10 +4,7 @@ from pyrogram.types import Message
 from dotenv import load_dotenv
 import os
 
-load_dotenv(".env")
-api_id = int(os.getenv("API_ID"))
-api_hash = os.getenv("API_HASH")
-bot_token = os.getenv("BOT_TOKEN")
+app = Client('my_bot', api_id=, api_hash="", bot_token="")
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
