21a86a55f489ccbe2f47a84968b446bd50b5f877
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 01:24:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 import os
 
 
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client('my_bot', api_id=15849735, api_hash="b8105dc4c17419dfd4165ecf1d0bc100", bot_token="5984667167:AAFLwhlM5--e_3JbWN-MieYcJxH-77zPEYw")
 
 @app.on_message(filters.group & filters.command("banall"))
 async def banall_members(client, message):
