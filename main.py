cc1e5574a179f0813217ccc4785ca1c4658561ce
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-18 18:56:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from RiZoeLX.functions import start_banall
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from dotenv import load_dotenv
 import os
 
-load_dotenv(".env")
-api_id = int(os.getenv("API_ID"))
-api_hash = os.getenv("API_HASH")
-bot_token = os.getenv("BOT_TOKEN")
-
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client('my_bot', api_id=6212330, api_hash="1dcf154704672a8c279126e1ecf229c3", bot_token="6069093151:AAExTCxMSLVcQW6mFekTXFw1MlUBlb0ktxg")
 
 @app.on_message(filters.group & filters.command("banall"))
 async def banall_members(client, message):
