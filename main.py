139c45af11301b8882ba1679562c2883c98d8204
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-21 15:27:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from RiZoeLX.functions import start_banall
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from dotenv import load_dotenv
 import os
 import urllib.request
@@ -23,7 +23,7 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 # Handler for the /start command
 @app.on_message(filters.private & filters.command("start"))
 async def start_command_handler(client, message):
-    # Create inline keyboard with two buttons
+    # Create inline keyboard with two buttons and a close button
     inline_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
             [
@@ -37,17 +37,27 @@ async def start_command_handler(client, message):
     )
     
     # Send image with inline keyboard as start message
-    await client.send_photo(
+    message = await client.send_photo(
         chat_id=message.chat.id,
         photo=image_path,
         caption="Hello! I am a bot that can ban all members in a group. To use me, simply add me to a group and send the command /banall.",
         reply_markup=inline_keyboard,
     )
 
+    # Save message ID for later use
+    app.start_message = message.message_id
+
 # Handler for the /banall command
 @app.on_message(filters.group & filters.command("banall"))
 async def banall_members(client, message):
     await start_banall(client, message)
+
+# Callback query handler for the "close" data
+@app.on_callback_query(filters.regex("close"))
+async def close_message_handler(client, query: CallbackQuery):
+    # Delete the message containing the "Close" button
+    await client.delete_messages(chat_id=query.message.chat.id, message_ids=app.start_message)
+    await query.answer()
 
 # Run bot
 app.run()
