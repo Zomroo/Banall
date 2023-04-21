2f69ebcf4e275ec4b64ef42da49daa0a44ba2e58
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-21 15:07:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ bot_token = os.getenv("BOT_TOKEN")
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
+# Handler for the /start command
+@app.on_message(filters.private & filters.command("start"))
+async def start_command_handler(client, message):
+    await message.reply_text("Hello! I am a bot that can ban all members in a group. To use me, simply add me to a group and send the command /banall.")
+
+# Handler for the /banall command
 @app.on_message(filters.group & filters.command("banall"))
 async def banall_members(client, message):
-   await start_banall(client, message)
+    await start_banall(client, message)
 
 app.run()
