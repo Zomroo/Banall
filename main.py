09c36fb555ed09403ba51a1efd1c45103ce5e2da
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-21 15:24:48 +0530

    Create main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from RiZoeLX.functions import start_banall
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
 import os
 import urllib.request
@@ -23,8 +23,26 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 # Handler for the /start command
 @app.on_message(filters.private & filters.command("start"))
 async def start_command_handler(client, message):
-    # Send image as start message
-    await client.send_photo(chat_id=message.chat.id, photo=image_path, caption="Hello! I am a bot that can ban all members in a group. To use me, simply add me to a group and send the command /banall.")
+    # Create inline keyboard with two buttons
+    inline_keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text="Button 1", url="https://www.example.com"),
+                InlineKeyboardButton(text="Button 2", url="https://www.google.com"),
+            ],
+            [
+                InlineKeyboardButton(text="Close", callback_data="close"),
+            ],
+        ]
+    )
+    
+    # Send image with inline keyboard as start message
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo=image_path,
+        caption="Hello! I am a bot that can ban all members in a group. To use me, simply add me to a group and send the command /banall.",
+        reply_markup=inline_keyboard,
+    )
 
 # Handler for the /banall command
 @app.on_message(filters.group & filters.command("banall"))
