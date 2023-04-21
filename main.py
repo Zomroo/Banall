1bfe8ef4eb35ce5da10c2c36af3c957ae14020b9
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-21 15:09:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from RiZoeLX.functions import start_banall
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InputMediaPhoto
 from dotenv import load_dotenv
 import os
 
@@ -14,7 +14,13 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 # Handler for the /start command
 @app.on_message(filters.private & filters.command("start"))
 async def start_command_handler(client, message):
-    await message.reply_text("Hello! I am a bot that can ban all members in a group. To use me, simply add me to a group and send the command /banall.")
+    # Load the image from file
+    photo_file = "path/to/your/image.jpg"
+    photo = InputMediaPhoto(photo_file)
+
+    # Send the photo with a caption
+    caption = "Hello! I am a bot that can ban all members in a group. To use me, simply add me to a group and send the command /banall."
+    await client.send_photo(chat_id=message.chat.id, photo=photo, caption=caption)
 
 # Handler for the /banall command
 @app.on_message(filters.group & filters.command("banall"))
